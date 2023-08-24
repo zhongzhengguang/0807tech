c170c100cee0f24ecf55b98671eadd5054becd3f
zhongzhengguang <paul930237@gmail.com>
2023-08-24 15:23:08 +0800

    123

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,5 +1,10 @@
+import Image from "next/image";
 import React from "react";
-
+import Banner1 from "@/public/Banner1.png";
 export function Banner() {
-  return <div className="w-full bg-pink-200 h-[200px]">123</div>;
+  return (
+    <div className="w-full bg-pink-200 h-[520px]">
+      <Image src={Banner1} alt="/" className=" bg-cover w-full h-full" />
+    </div>
+  );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <div className=" w-full bg-[#666] h-full mt-[10px] text-white px-20 py-20 flex justify-between items-center">
+      <div className="bg-pink-200 w-[500px] h-[500px]"></div>
+      <div className="bg-pink-200 w-[500px] h-[500px]"></div>
+      <div className="bg-pink-200 w-[500px] h-[500px]"></div>
+    </div>
+  );
+}
