37047515ed4914dcbb149283f53cd57d96fe8384
鍾政廣 <zhongzhengguang@zhongzhengguangdeMacBook-Pro-2.local>
2023-09-07 17:17:37 +0800

    SEO測試

--- pages/Calculation.tsx
+++ pages/Calculation.tsx
@@ -1,29 +1,25 @@
 import Image from "next/image";
 import React, { useState } from "react";
-import K from "@/public/K.png";
-import TH from "@/public/TH.png";
-import TC from "@/public/TC.png";
-import Q from "@/public/Q.png";
-import Q1234 from "@/public/Q1234.png";
+
 function Calculation() {
     const [show, setShow] = useState(false);
     return (
         <div className="pt-20 w-[1280px] mx-auto">
             <div className="flex justify-center mt-[100px] space-x-[100px]">
                 <div onClick={() => setShow(!show)} className=" cursor-pointer">
-                    <Image src={K} alt="K" width={500} height={500} />
+                    {/* <Image src={K} alt="K" width={500} height={500} /> */}
                 </div>
                 {show && (
                     <div className="flex flex-col">
-                        <Image src={TH} alt="K" width={500} height={500} />
-                        <Image src={TC} alt="K" width={500} height={500} />
+                        {/* <Image src={TH} alt="K" width={500} height={500} />
+                        <Image src={TC} alt="K" width={500} height={500} /> */}
                     </div>
                 )}
             </div>
             {show && (
                 <div className="flex justify-center mt-[100px] space-x-[100px]">
-                    <Image src={Q} alt="q" width={500} height={500} />
-                    <Image src={Q1234} alt="q1234" width={500} height={500} />
+                    {/* <Image src={Q} alt="q" width={500} height={500} />
+                    <Image src={Q1234} alt="q1234" width={500} height={500} /> */}
                 </div>
             )}
         </div>
